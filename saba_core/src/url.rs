1b0bc912d46d7549f6a701263cdac5bb47f78946
sada0918 <shota.0918.fw@gmail.com>
2025-12-05 17:44:30 +0900

    change: Urlのコンストラクタとパースメソッド（異常系のみ）を追加

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -8,3 +8,29 @@ pub struct Url {
     path: String,
     searchpart: String,
 }
+
+impl Url {
+    pub fn new(url: String) -> Self {
+        Self {
+            url,
+            host: "".to_string(),
+            port: "".to_string(),
+            path: "".to_string(),
+            searchpart: "".to_string(),
+        }
+    }
+
+    pub fn parse(&mut self) -> Result<Self, String> {
+        if !self.is_http() {
+            return Err("Only HTTP scheme is supported.".to_string());
+        }
+        // 正常系はこの後実装
+    }
+
+    fn is_http(&mut self) -> bool {
+        if self.url.contains("http://") {
+            return true;
+        }
+        false
+    }
+}
