119c42d02c251b21f555780bfe0490b8083a2bec
sada0918 <shota.0918.fw@gmail.com>
2025-12-05 18:06:03 +0900

    change: hostを抽出する関数を作成

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -35,4 +35,17 @@ impl Url {
         }
         false
     }
+
+    fn extract_host(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+        if let Some(index) = url_parts[0].find(':') {
+            url_parts[0][..index].to_string()
+        } else {
+            url_parts[0].to_string()
+        }
+    }
 }
