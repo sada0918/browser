32bb116b6a472acf3d68b1e456db7e5e9f032e45
sada0918 <shota.0918.fw@gmail.com>
2025-12-07 22:10:51 +0900

    change: 使用予定のインポート追加と、GETメソッドのシグネチャを作成

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -1,7 +1,16 @@
+extern crate alloc;
+use alloc::string::String;
+use saba_core::error::Error;
+use saba_core::http::HttpResponse;
+
 pub struct HttpClient {}
 
 impl HttpClient {
     pub new() -> Self {
         Self {}
+    }
+
+    pub fn get($self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
+        // あとで実装する
     }
 }
