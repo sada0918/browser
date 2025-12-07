16ae1f8d55b77658c8ac02af7b6e3be48c0272b1
sada0918 <shota.0918.fw@gmail.com>
2025-12-07 23:42:15 +0900

    change: ヘッダーの構造体とコンストラクタを追加

--- saba_core/src/http.rs
+++ saba_core/src/http.rs
@@ -2,6 +2,18 @@ use alloc::string::String;
 use alloc::vec::Vec;
 
 #[derive(Debug, Clone)]
+pub struct Header {
+    name: String,
+    value: String,
+}
+
+impl Header {
+    pub fn new(name: String, value: String) -> Self {
+        Self {name, value}
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct HttpResponse {
     version: String,
     status_code: u32,
