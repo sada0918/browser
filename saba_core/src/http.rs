d113beef324344472ee42b91d02cad9bcf1f7108
sada0918 <shota.0918.fw@gmail.com>
2025-12-08 00:28:10 +0900

    change: 文字列型のレスポンスをHttpレスポンス構造体にするためのコンストラクタを作成

--- saba_core/src/http.rs
+++ saba_core/src/http.rs
@@ -1,5 +1,8 @@
 use alloc::string::String;
 use alloc::vec::Vec;
+use alloc::format,
+use crate::error::Error;
+use crate::alloc::string::ToString;
 
 #[derive(Debug, Clone)]
 pub struct Header {
@@ -20,4 +23,45 @@ pub struct HttpResponse {
     reason: String,
     headers: Vec<Header>,
     body: String,
+}
+
+impl HttpResponse {
+    pub fn new(raw_response: String) -> Result<Self, Error> {
+        let preprocessed_response = raw_response.trim_start().replace("\n\r", "\n");
+
+        let (status_line, remaining) = match preprocessed_response.split_once('\n') {
+            Some((s,  r)) => (s, r),
+            None => {
+                return Err(Error::Network(format!(
+                    "invalid http response: {}",
+                    preprocessed_response
+                )))
+            }
+        }
+
+        let (headers, body) = match remaining.split_once("\n\n") {
+            Some((h, b)) => {
+                let mut headers = Vec::new();
+                for header in h.split('\n') {
+                    let splitted_header: Vec<&str> = header.splitn(2, ':').collect();
+                    headers.push(Header::new(
+                        String::from(splitted_header[0].trim()),
+                        String::from(splitted_header[1].trim()),
+                    ));
+                }
+                (headers, b)
+            },
+            None => {Vec::new(), remaining}
+        }
+
+        let statuses: Vec<&str> = status_line.split(' ').collect();
+
+        Ok(Self {
+            version: statuses[0].to_string(),
+            status_code: statuses[1].parse().unwrap_or(404),
+            reason: statuses[2].to_string();
+            headers,
+            body: body.to_string();
+        })
+    }
 }
